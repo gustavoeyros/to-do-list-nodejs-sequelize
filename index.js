135f132d6cc54d29f67dd67e2979762093c12d76
Gustavo Neves <gustavoeyros@hotmail.com>
2022-10-08 23:39:52 -0300

    update & delete

--- index.js
+++ index.js
@@ -10,9 +10,35 @@ app.use(express.urlencoded({
     extended: true
 }))
 
+//Inserir a mensagem
 app.post('/message/create', async(req, res)=>{
     const description = req.body.description
     await Message.create({description})
+    res.redirect('/')
+})
+
+app.get('/message/edit/:id', async(req, res)=>{
+    const id = req.params.id
+    const message = await Message.findOne({raw: true, where: {id:id}})
+    res.render('edit', {message})
+})
+
+app.post('/message/update/', async(req, res)=>{
+    const id = req.body.id
+    const description = req.body.description
+    const listUpdate = {
+        id,
+        description
+    }
+    const update = await Message.update(listUpdate, {where: {id:id}})
+    res.redirect('/')
+})
+
+
+//Apagar a mensagem
+app.get('/message/delete/:id', async(req, res)=>{
+    const id = req.params.id
+    await Message.destroy({where: {id: id}})
     res.redirect('/')
 })
 
